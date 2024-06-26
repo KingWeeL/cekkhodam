e41db7619a528d44bf622ad31b8a1c1a71961c0c
KingWeeL <138101088+KingWeeL@users.noreply.github.com>
2024-06-26 11:50:25 +0700

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('khodamForm').addEventListener('submit', function(e) {
     e.preventDefault();
-    const name = document.getElementById('name').value;
+    const name = document.getElementById('name').value.trim();
     const khodamData = {
         'Khodam Harimau sumatra': 'Melambangkan kekuatan dan keberanian yang luar biasa.',
         'Khodam Harimau': 'Menandakan ketangguhan dan keberanian dalam menghadapi tantangan.',
@@ -17,15 +17,23 @@ document.getElementById('khodamForm').addEventListener('submit', function(e) {
         'Khodam Harimau Putih': 'Melambangkan kekuatan dan kemurnian.',
         'Khodam Biawak': 'Simbol kelicikan dan kemampuan beradaptasi.',
         'Kak Gem': 'Orang yg memiliki khodam ini memiliki kelebihan yang suka memberi paham',
+        'Khodam Belut pemalas': 'Simbol kemalasan yang kuat.',
         '': 'Tidak ada Khodam yang terdeteksi.'
     };
-    
-    const randomKhodam = Object.keys(khodamData)[Math.floor(Math.random() * Object.keys(khodamData).length)];
+
+    let khodam;
+    if (localStorage.getItem(name)) {
+        khodam = localStorage.getItem(name);
+    } else {
+        khodam = Object.keys(khodamData)[Math.floor(Math.random() * Object.keys(khodamData).length)];
+        localStorage.setItem(name, khodam);
+    }
+
     const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = `<p>Nama Anda: ${name}</p><p>Jenis Khodam Anda: ${randomKhodam}</p><p>Makna Khodam: ${khodamData[randomKhodam]}</p>`;
+    resultDiv.innerHTML = `<p>Nama Anda: ${name}</p><p>Jenis Khodam Anda: ${khodam}</p><p>Makna Khodam: ${khodamData[khodam]}</p>`;
 
     // Membaca teks dengan suara
-    const textToSpeak = `Nama Anda: ${name}. Jenis Khodam Anda: ${randomKhodam}. Makna Khodam: ${khodamData[randomKhodam]}`;
+    const textToSpeak = `Nama Anda: ${name}. Jenis Khodam Anda: ${khodam}. Makna Khodam: ${khodamData[khodam]}`;
     const speech = new SpeechSynthesisUtterance(textToSpeak);
     speech.lang = 'id-ID'; // Set bahasa ke bahasa Indonesia
     window.speechSynthesis.speak(speech);
